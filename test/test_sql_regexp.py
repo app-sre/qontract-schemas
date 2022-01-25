7877d2249d9f7f94511604642cefb5bc7cc8e3bd
Jordi Piriz <jpiriz@redhat.com>
2022-01-25 14:05:52 +0100

    Improve regexp

--- test/test_sql_regexp.py
+++ test/test_sql_regexp.py
@@ -14,13 +14,16 @@ valid_sql_queries = [
     "select a from ( select a,b from table_b );",
     "explain select a from table_a;",
     "explain analyze select a from table_a;",
-    "SELECT a from table b where id ='3ec6f667-4083**';"
+    "SELECT a from table b where id ='3ec6f667-4083**';",
+    """select a from table -- order by id
+     order by id;""",
+    "select\n  -- get things that appeared to be running on 2021-05-17\n  -- user/clount info\n  au.id as django_user_id,\n  ca.id as cloud_account_id,\n  aca.id as aws_cloud_account_id,\n  -- instance info\n  i.id as instance_id,\n  ai.id as aws_instance_id,\n  ai.ec2_instance_id as ec2_instance_id,\n  -- image info\n  mi.id as machine_image_id,\n  mi.name as machine_image_name,\n  mi.status as machine_image_status,\n  ami.id as aws_machine_image_id,\n  ami.ec2_ami_id as ec2_ami_id,\n  -- run info\n  r.id as run_id,\n  r.start_time as run_start_time\nfrom\n  auth_user au\n  join api_cloudaccount ca on au.id = ca.user_id\n  join api_awscloudaccount aca on aca.id = ca.object_id\n  join api_instance i on i.cloud_account_id = ca.id\n  join api_awsinstance ai on ai.id = i.object_id\n  join api_run r on r.instance_id = i.id\n  join api_machineimage mi on mi.id = r.machineimage_id\n  join api_awsmachineimage ami on ami.id = mi.object_id\nwhere\n  username = '6362142' -- the qe account used for longevity tests\n  username = '6362142'\n  AND r.start_time <= '2021-05-17 00:00:00'\n  AND coalesce(r.end_time, '2021-05-17 00:00:01') > '2021-05-17 00:00:00'\norder by\n  -- most recently started runs first\n  r.start_time desc;\n"
+
 ]
 
 not_valid_sql_queries = [
     "select * from table_a;",
     "SELECT * FROM TABLE_A;",
-    "select a from table -- order by id order by id;",
     "select a from ( select * from table_b );",
     "select a from ( select /*comment*/* from table_b );",
     "explain analyze update table set a='b' where id=1;",
